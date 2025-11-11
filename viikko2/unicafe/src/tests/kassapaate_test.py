11268ac4aa7273872534a20041f05d18e1e65d57
topias t <topias.tallberg@gmail.com>
2025-11-11 21:59:54 +0200

    tehtävä 9 done

--- viikko2/unicafe/src/tests/kassapaate_test.py
+++ viikko2/unicafe/src/tests/kassapaate_test.py
@@ -2,6 +2,7 @@ import unittest
 from kassapaate import Kassapaate
 from maksukortti import Maksukortti
 
+
 class TestKassapaate(unittest.TestCase):
     def setUp(self):
         self.kassapaate = Kassapaate()
